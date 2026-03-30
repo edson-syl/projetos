851c61cc2d01c1ef58cf3f1642c2993fb0be2d84
Edson <edsonsylva.bat@yahoo.com>
2026-03-30 15:39:45 -0300

    feat : update Cardapio

--- frontend/Cardapio Digital/src/pages/Home.jsx
+++ frontend/Cardapio Digital/src/pages/Home.jsx
@@ -1,37 +1,36 @@
 import { products } from "../data/products"
-
 import ProductCard from "../components/ProductCard"
 
 function Home() {
-  const params = new URLSearchParams(window.location.search)
-  const mesa = params.get("mesa")
+const params = new URLSearchParams(window.location.search)
+const mesa = params.get("mesa")
 
-  return (
-    <div className="bg-zinc-950 min-h-screen text-zinc-100 px-5 py-6">
-      
-      <header className="mb-8">
-        <h1 className="text-3xl font-semibold tracking-tight">
-          Cardápio
-        </h1>
+return ( 
+  <div className="bg-zinc-950 min-h-screen text-zinc-100 px-5 py-6 pb-26">
+    <header className="mb-8">
+      <h1 className="text-3xl font-semibold tracking-tight">
+        Cardápio
+      </h1>
 
-        {mesa && (
-          <p className="text-zinc-400 text-sm mt-1">
-            Mesa {mesa}
-          </p>
-        )}
-
-        <p className="text-zinc-500 text-sm">
-          Faça seu pedido
+      {mesa && (
+        <p className="text-zinc-400 text-sm mt-1">
+          Mesa {mesa}
         </p>
-      </header>
+      )}
 
-      <div className="grid grid-cols-2 gap-4">
-        {products.map((item) => (
-          <ProductCard key={item.id} product={item} />
-        ))}
-      </div>
+      <p className="text-zinc-500 text-sm">
+        Faça seu pedido
+      </p>
+    </header>
 
+    <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+      {products.map((item) => (
+        <ProductCard key={item.id} product={item} />
+      ))}
     </div>
+
+  </div>
+
   )
 }
 
